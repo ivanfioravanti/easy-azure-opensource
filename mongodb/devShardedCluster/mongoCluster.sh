380fcfb9509681fddf006b3180f1ba5768e5d5a1
Ivan Fioravanti <ivan.fioravanti@4ward.it>
2015-03-03 16:13:15 +0100

    StorageEngine option added and wiredTiger used by default

--- mongodb/devShardedCluster/mongoCluster.sh
+++ mongodb/devShardedCluster/mongoCluster.sh
@@ -2,17 +2,19 @@
 DATAFOLDER=/mnt/data
 NUMOFSHARDS=4
 MONGOUSER=azureuser
+# feel free to use mmapv1 or wiredTiger
+STORAGEENGINE=wiredTiger
 
 case "$1" in
     start)
         for i in $(seq -f "%02g" 0 $((NUMOFSHARDS-1)))
         do
            echo "\n\nStarting Shard $i.\n"
-           mongod --port 270$i --dbpath $DATAFOLDER/s$i --smallfiles --oplogSize 128 --fork --logpath $DATAFOLDER/s$i/s$i.log        
+           mongod --port 270$i --dbpath $DATAFOLDER/s$i --storageEngine $STORAGEENGINE --smallfiles --oplogSize 128 --fork --logpath $DATAFOLDER/s$i/s$i.log        
         done
 
         echo "\n\nStarting Configuration Servers.\n"
-        mongod --configsvr --dbpath $DATAFOLDER/conf/conf0 --port 26000 --fork --logpath $DATAFOLDER/conf/conf0.log
+        mongod --configsvr --dbpath $DATAFOLDER/conf/conf0 --storageEngine $STORAGEENGINE --port 26000 --fork --logpath $DATAFOLDER/conf/conf0.log
         
         echo "\n\nWaiting before starting the Query Router.\n"
         sleep 25
